661e146ebf5d000a3828b73ca5ae234ea050b2b7
jobinku <65527360+jobinku@users.noreply.github.com>
2020-05-19 21:34:47 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,21 +4,24 @@ This is a Telegram Rename Bot!
 
 Please send me any Telegram file and reply to that file to /rename New Name.mkv
 
-Channel : © @MOVIES_NIGHTG
 
-/help for more details.."""
+Channel :  @MOVIES_NIGHTG
+
+/help for more details
+
+Owner © @MuSkysM"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    DOWNLOAD_START = "📥 downloading"
-    UPLOAD_START = "📥 uploading"
+    DOWNLOAD_START = "📥 download"
+    UPLOAD_START = "📥 upload"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@MuSkysM</a>"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Thumbnail saved ✅."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✔ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = " "
@@ -29,11 +32,11 @@ Channel : © @MOVIES_NIGHTG
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension.
    
-<b>👉 casting owner :</b> 👉 <a href="https://t.me/MuSkysM"""
+<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
 
 --------
 
-Support Group : © @MOVIENIGHTGROUP,@MOVIES_NIGHTG"""
+Support Group : @MOVIENIGHTGROUP,@MOVIES_NIGHTG"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
