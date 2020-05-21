06681df0a7f5d1aa85ed5a8d4ea27444a9fd4db4
jobinku <65527360+jobinku@users.noreply.github.com>
2020-05-21 19:41:49 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -43,7 +43,7 @@ async def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join(["⚫" for i in range(math.floor(percentage / 4))]),
+            ''.join(["🔵" for i in range(math.floor(percentage / 4))]),
             ''.join(["🔴" for i in range(20 - math.floor(percentage / 4))]),
             round(percentage, 2))
 
